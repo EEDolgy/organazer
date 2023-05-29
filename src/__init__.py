16e1532702066376b632e2a5168598e26719be3a
EEDolgy <eedolgy@gmail.com>
2023-05-30 02:39:04 +0300

    Basik html structure and database creation have been added

--- src/__init__.py
+++ src/__init__.py
@@ -1,20 +1,38 @@
 from flask import Flask
-from .general import general
-from .auth import auth
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'Helloworld'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .general import general
+    from .auth import auth
 
     app.register_blueprint(general, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    # login_manager = LoginManager()
-    # login_manager.login_view = "auth.login"
-    # login_manager.init_app(app)
-    #
-    # @login_manager.user_loader
-    # def load_user(id):
-    #     return id
+    from .models import User
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
+
+def create_database(app):
+    if not path.exists('src/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
