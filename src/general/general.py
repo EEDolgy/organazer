8b22678e84fc6993264f49704e150cbcd597780c
EEDolgy <eedolgy@gmail.com>
2023-06-01 01:47:27 +0300

    project structure simplified

--- src/general/general.py
+++ src/general/general.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 from ..models import Content
+from .. import db
+
 
 general = Blueprint("general", __name__, template_folder='templates')
 
@@ -12,6 +14,25 @@ def greetings():
 @general.route("/home")
 @login_required
 def home():
-    contents = Content.query.all()
-    return render_template("general/home.html", user=current_user, contents=contents)
+    notes = Content.query.all()
+    return render_template("general/home.html", user=current_user, notes=notes)
+
+@general.route("/add-note", methods=['POST', 'GET'])
+@login_required
+def add_note():
+    if request.method == 'POST':
+        text = request.form.get('text')
+
+        if not text:
+            flash('A note can not be empty', category='error')
+        else:
+            cont = Content(text=text, author=current_user.id)
+
+            db.session.add(cont)
+            db.session.commit()
+
+            flash('Note created', category='success')
+
+
+    return render_template("content/add-note.html", user=current_user)
 
